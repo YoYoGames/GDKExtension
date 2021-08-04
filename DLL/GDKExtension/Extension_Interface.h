c6bdc1d01c369ce9c5c645c74fc11519942df740
Russell Kay <russell.kay@yoyogames.com>
2021-08-04 22:15:14 +0100

    Added latest YYRunnerInterface
    * ensure interface is copied over
    * added more example to the exported function

--- DLL/GDKExtension/Extension_Interface.h
+++ DLL/GDKExtension/Extension_Interface.h
@@ -3,18 +3,131 @@
 
 #include <stdint.h>
 
+struct RValue;
+class YYObjectBase;
+class CInstance;
+struct YYRunnerInterface;
+typedef void (*TSetRunnerInterface)(const YYRunnerInterface* pRunnerInterface, size_t _functions_size);
+typedef void (*TYYBuiltin)(RValue& Result, CInstance* selfinst, CInstance* otherinst, int argc, RValue* arg);
+
 struct YYRunnerInterface
 {
-	void (*DebugConsoleOutput)(const char* fmt, ...);
+	// basic interaction with the user
+	void (*DebugConsoleOutput)(const char* fmt, ...); // hook to YYprintf
 	void (*ShowMessage)(const char* msg);
 
-	void (*CreateAsyncEventWithDSMap)(int, int);
+	// for printing error messages
+	void (*YYError)(const char* _error, ...);
+
+	// alloc, realloc and free
+	void* (*YYAlloc)(int _size);
+	void* (*YYRealloc)(void* pOriginal, int _newSize);
+	void  (*YYFree)(const void* p);
+
+	// yyget* functions for parsing arguments out of the arg index
+	bool (*YYGetBool)(const RValue* _pBase, int _index);
+	float (*YYGetFloat)(const RValue* _pBase, int _index);
+	double (*YYGetReal)(const RValue* _pBase, int _index);
+	int32_t(*YYGetInt32)(const RValue* _pBase, int _index);
+	uint32_t(*YYGetUint32)(const RValue* _pBase, int _index);
+	int64_t(*YYGetInt64)(const RValue* _pBase, int _index);
+	void* (*YYGetPtr)(const RValue* _pBase, int _index);
+	intptr_t(*YYGetPtrOrInt)(const RValue* _pBase, int _index);
+	const char* (*YYGetString)(const RValue* _pBase, int _index);
+
+	// typed get functions from a single rvalue
+	bool (*BOOL_RValue)(const RValue* _pValue);
+	double (*REAL_RValue)(const RValue* _pValue);
+	void* (*PTR_RValue)(const RValue* _pValue);
+	int64_t(*INT64_RValue)(const RValue* _pValue);
+	int32_t(*INT32_RValue)(const RValue* _pValue);
+
+	// calculate hash values from an RValue
+	int (*HASH_RValue)(const RValue* _pValue);
+
+	// copying, setting and getting RValue
+	void (*SET_RValue)(RValue* _pDest, RValue* _pV, YYObjectBase* _pPropSelf, int _index);
+	bool (*GET_RValue)(RValue* _pRet, RValue* _pV, YYObjectBase* _pPropSelf, int _index, bool fPrepareArray, bool fPartOfSet);
+
+	// finding and runnine user scripts from name
+	int (*Script_Find_Id)(const char* name);
+	bool (*Script_Perform)(int ind, CInstance* selfinst, CInstance* otherinst, int argc, RValue* res, RValue* arg);
+
+	// finding builtin functions
+	bool  (*Code_Function_Find)(const char* name, int* ind);
+
+	// ds map manipulation for 
+	void (*CreateAsyncEventWithDSMap)(int _map, int _event);
 	int (*CreateDsMap)(int _num, ...);
 
 	bool (*DsMapAddDouble)(int _index, const char* _pKey, double value);
 	bool (*DsMapAddString)(int _index, const char* _pKey, const char* pVal);
 	bool (*DsMapAddInt64)(int _index, const char* _pKey, int64_t value);
 };
+
+
+#if defined(__YYDEFINE_EXTENSION_FUNCTIONS__)
+extern YYRunnerInterface* g_pYYRunnerInterface;
+
+// basic interaction with the user
+#define DebugConsoleOutput(fmt, ...)		g_pYYRunnerInterface->DebugConsolOutput(fmt, __VA_ARGS__  )
+void ShowMessage(const char* msg) { g_pYYRunnerInterface->ShowMessage(msg); }
+
+// for printing error messages
+#define YYError(_error, ...)				g_pYYRunnerInterface->YYError( _error, __VA_ARGS__ )
+
+// alloc, realloc and free
+void* YYAlloc(int _size) { return g_pYYRunnerInterface->YYAlloc(_size); }
+void* YYRealloc(void* pOriginal, int _newSize) { return g_pYYRunnerInterface->YYRealloc(pOriginal, _newSize); }
+void  YYFree(const void* p) { g_pYYRunnerInterface->YYFree(p); }
+
+// yyget* functions for parsing arguments out of the arg index
+bool YYGetBool(const RValue* _pBase, int _index) { return g_pYYRunnerInterface->YYGetBool(_pBase, _index); }
+float YYGetFloat(const RValue* _pBase, int _index) { return g_pYYRunnerInterface->YYGetFloat(_pBase, _index); }
+double YYGetReal(const RValue* _pBase, int _index) { return g_pYYRunnerInterface->YYGetReal(_pBase, _index); }
+int32_t YYGetInt32(const RValue* _pBase, int _index) { return g_pYYRunnerInterface->YYGetInt32(_pBase, _index); }
+uint32_t YYGetUint32(const RValue* _pBase, int _index) { return g_pYYRunnerInterface->YYGetUint32(_pBase, _index); }
+int64_t YYGetInt64(const RValue* _pBase, int _index) { return g_pYYRunnerInterface->YYGetInt64(_pBase, _index); }
+void* YYGetPtr(const RValue* _pBase, int _index) { return g_pYYRunnerInterface->YYGetPtr(_pBase, _index); }
+intptr_t YYGetPtrOrInt(const RValue* _pBase, int _index) { return g_pYYRunnerInterface->YYGetPtrOrInt(_pBase, _index); }
+const char* YYGetString(const RValue* _pBase, int _index) { return g_pYYRunnerInterface->YYGetString(_pBase, _index); }
+
+// typed get functions from a single rvalue
+bool BOOL_RValue(const RValue* _pValue) { return g_pYYRunnerInterface->BOOL_RValue(_pValue); }
+double REAL_RValue(const RValue* _pValue) { return g_pYYRunnerInterface->REAL_RValue(_pValue); }
+void* PTR_RValue(const RValue* _pValue) { return g_pYYRunnerInterface->PTR_RValue(_pValue); }
+int64_t INT64_RValue(const RValue* _pValue) { return g_pYYRunnerInterface->INT64_RValue(_pValue); }
+int32_t INT32_RValue(const RValue* _pValue) { return g_pYYRunnerInterface->INT32_RValue(_pValue); }
+
+// calculate hash values from an RValue
+int HASH_RValue(const RValue* _pValue) { return g_pYYRunnerInterface->HASH_RValue(_pValue); }
+
+// copying, setting and getting RValue
+void SET_RValue(RValue* _pDest, RValue* _pV, YYObjectBase* _pPropSelf, int _index) { return g_pYYRunnerInterface->SET_RValue(_pDest, _pV, _pPropSelf, _index); }
+bool GET_RValue(RValue* _pRet, RValue* _pV, YYObjectBase* _pPropSelf, int _index, bool fPrepareArray = false, bool fPartOfSet = false) { return g_pYYRunnerInterface->GET_RValue(_pRet, _pV, _pPropSelf, _index, fPrepareArray, fPartOfSet); }
+
+// finding and runnine user scripts from name
+int Script_Find_Id(char* name) { return g_pYYRunnerInterface->Script_Find_Id(name); }
+bool Script_Perform(int ind, CInstance* selfinst, CInstance* otherinst, int argc, RValue* res, RValue* arg) {
+	return g_pYYRunnerInterface->Script_Perform(ind, selfinst, otherinst, argc, res, arg);
+}
+
+// finding builtin functions
+bool  Code_Function_Find(char* name, int* ind) { return g_pYYRunnerInterface->Code_Function_Find(name, ind); }
+
+// ds map manipulation for 
+void CreateAsyncEventWithDSMap(int _map, int _event) { return g_pYYRunnerInterface->CreateAsyncEventWithDSMap(_map, _event); }
+#define CreateDsMap(_num, ...) g_pYYRunnerInterface->CreateDsMap( _num, __VA_ARGS__ )
+
+bool DsMapAddDouble(int _index, const char* _pKey, double value) { return g_pYYRunnerInterface->DsMapAddDouble(_index, _pKey, value); }
+bool DsMapAddString(int _index, const char* _pKey, const char* pVal) { return g_pYYRunnerInterface->DsMapAddString(_index, _pKey, pVal); }
+bool DsMapAddInt64(int _index, const char* _pKey, int64_t value) { return g_pYYRunnerInterface->DsMapAddInt64(_index, _pKey, value); }
+
+
+
+#endif
+
+
 
 #define YY_HAS_FUNCTION(interface, interface_size, function) \
 	(interface_size >= (offsetof(GameMaker_RunnerInterface, function) + sizeof(GameMaker_RunnerInterface::function)) && interface->function != NULL)
